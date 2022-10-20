6e775004f45610d194edef38f9549c77b58e1b5d
Abdul Rafi <arafi15@gmail.com>
2022-10-20 15:58:57 +0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+import UserForm from "./components/UserForm"
+import './app.css';
 
 const App = () => {
+
+  const [user, setUser] = useState()
+
+  console.log(user)
+
+  const getFormData = (name, age, DOB, contact, email, password) => {
+    setUser({
+      name,
+      age,
+      DOB,
+      contact,
+      email,
+      password
+    })
+  }
+
   return (
-    <div>
-      Hello World
+    <div className="container">
+      <UserForm getFormData ={getFormData}/>
     </div>
   )
 }
@@ -12,3 +30,5 @@ export default App
 
 //install propTypes
 //change Root error
+//create css file inside src
+//redo snippets for css
