82439082530cda8b7e249cd9dbadca7a655eeb87
Abdul Rafi <arafi15@gmail.com>
2022-10-20 20:29:51 +0800

    prep for friday

---
+++ week3-day5/src/app.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import NewComponent from "./components/NewComponent"
+import "./app.css"
+
+const App = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+export default App
+
+
+//change Root error
